f87a545431ec63b63234b764e2d7eff8f426d41d
AustinKarasu <zyronetworkofficial@gmail.com>
2026-03-27 21:55:55 +0530

    Delete frontend directory

--- frontend/src/api/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import { API_ENDPOINTS } from '../endpoints'
-import { apiClient } from '../http'
-
-export const dashboardService = {
-  async getSummary() {
-    const response = await apiClient.get(API_ENDPOINTS.dashboard.summary)
-    return response.data
-  },
-}
--- frontend/src/api/services/leaderboardService.js
+++
@@ -1,9 +0,0 @@
-import { API_ENDPOINTS } from '../endpoints'
-import { apiClient } from '../http'
-
-export const leaderboardService = {
-  async getSummary() {
-    const response = await apiClient.get(API_ENDPOINTS.leaderboard.summary)
-    return response.data
-  },
-}
